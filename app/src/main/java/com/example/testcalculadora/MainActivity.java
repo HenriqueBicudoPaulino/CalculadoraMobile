778c0b23ce10efbbee80ef721f50675ca1b4ff81
Henrique Bicudo Paulino <hbicudo.hb@gmail.com>
2025-09-10 19:28:38 -0300

    Leyout creation following the wireframe

--- app/src/main/java/com/example/testcalculadora/MainActivity.java
+++ app/src/main/java/com/example/testcalculadora/MainActivity.java
@@ -1,5 +1,12 @@
 package com.example.testcalculadora;
-import android.app.Activity;
 
-public class MainActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.telaInicial.xml);
+    }
 }
